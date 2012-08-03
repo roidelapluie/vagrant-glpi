cd6b84890b43cb59dcbf5b130ba720e80a647935
Julien Pivotto <roidelapluie@inuits.eu>
2012-08-03 17:45:50 +0200

    include apache module in site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,7 +1,11 @@
 node default {
   include site::repositories
+
+  include apache::params
+  include apache
+
   class {
     'glpi':
-      require => Class['site::repositories'],
+      require => Class['site::repositories', 'apache::params', 'apache'],
   }
 }
