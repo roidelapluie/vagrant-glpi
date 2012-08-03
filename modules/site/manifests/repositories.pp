59314a666357f21eedf510ba1407302d8d843bee
Julien Pivotto <roidelapluie@inuits.eu>
2012-08-03 17:35:06 +0200

    fix case and yumrepo resource in repositories definition

--- modules/site/manifests/repositories.pp
+++ modules/site/manifests/repositories.pp
@@ -1,9 +1,9 @@
 class site::repositories {
-  case $::operatingsystem ? {
+  case $::operatingsystem {
     'CentOS', 'RedHat': {
       yumrepo {
         'remi':
-          desc     => 'Les RPM de remi pour Enterprise Linux',
+          descr    => 'Les RPM de remi pour Enterprise Linux',
           baseurl  => 'http://remi-mirror.dedipower.com/enterprise/$releasever/remi/x86_64/',
           enabled  => 1,
           gpgcheck => 0,
